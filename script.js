13be8adcc7a64362ebe0e20d2e3f388ea4ec0554
David Stewart <davidstewy@gmail.com>
2018-05-22 15:19:55 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+redPlayer = true;
+blackPlayer = false;
+
+let board = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+];
+
+
+if (redPlayer === true) {
+
+    redPlayer = false;
+    blackPlayer = true;
+
+} else {
+    blackPlayer = false;
+    redPlayer = true;
+}
