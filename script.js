cf3dcd3934fb402053d3e07d0c2e1867e2005c59
David Stewart <davidstewy@gmail.com>
2018-09-25 15:56:44 -0400

    added some win conditions

--- script.js
+++ script.js
@@ -1,22 +1,49 @@
-redPlayer = true;
-blackPlayer = false;
+let board = [[], [], [], [], [], [], []];
 
-let board = [
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0]
-];
+let divsToClick = document.querySelectorAll(".columns");
+var click = 0;
 
+function placeChip(event) {
+  let target = event.currentTarget;
+  let div = document.createElement("div");
+  console.log(target.dataset.colIndex);
 
-if (redPlayer === true) {
+  if (target.children.length < 6) {
+    click++;
 
-    redPlayer = false;
-    blackPlayer = true;
+    if (click % 2 == 0) {
+      div.classList.add("black");
+      board[target.dataset.colIndex].push("Black");
+    } else {
+      div.classList.add("red");
+      board[target.dataset.colIndex].push("Red");
+    }
 
-} else {
-    blackPlayer = false;
-    redPlayer = true;
+    target.appendChild(div);
+    setTimeout(() => board.forEach(winningCol), 0);
+    console.log(click);
+  }
 }
+
+for (let i = 0; i < divsToClick.length; i++) {
+  let col = divsToClick[i];
+  col.addEventListener("click", placeChip);
+}
+
+function winningCol(winningPiecesColumn) {
+  let lastSeenPiece = "";
+  let count = 0;
+
+  winningPiecesColumn.forEach(piece => {
+    if (lastSeenPiece === piece) {
+      count++;
+      if (count === 4) {
+        alert("You Win " + piece);
+      }
+    } else {
+      count = 1;
+      lastSeenPiece = piece;
+    }
+  });
+}
+/* get x coord with datasets, y coord with the length of the index of the array */
